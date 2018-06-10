776194390275f579654ad9a9fda217d55c2674ff
Chickee Pante-Fuerman <frances.chickee@gmail.com>
2018-06-10 12:28:40 -0400

    Push through even if Im not yet sure about my AS32 status- still flickering and not sure if I have properly replaced the codes that needed replacement of setSong and getSongNumberCell. Esp lines 118 and 145.

--- scripts/album.js
+++ scripts/album.js
@@ -1,10 +1,27 @@
 var setSong = function (songNumber){
-  currentlyPlayingSongNumber = parseInt(songNumber);
-  currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    if (currentSoundFile) {
+         currentSoundFile.stop();
+    }
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    // #1
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        // #2
+        formats: [ 'mp3' ],
+        preload: true
+    });
+
+    setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
 };
 
 var getSongNumberCell = function(number){
-  return $('.song-item-number[data-song-number="' + number + '"]');
+    return $('.song-item-number[data-song-number="' + number + '"]');
 };
 
 var createSongRow = function(songNumber, songName, songLength) {
@@ -47,13 +64,18 @@ var createSongRow = function(songNumber, songName, songLength) {
         		// Switch from Play -> Pause button to indicate new song is playing.
         		$(this).html(pauseButtonTemplate);
         		setSong(songNumber);
+            currentSoundFile.play();
             updatePlayerBarSong();
       	} else if (currentlyPlayingSongNumber === songNumber) {
-        		// Switch from Pause -> Play button to pause currently playing song.
-        		$(this).html(playButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPlayButton);
-        		currentlyPlayingSongNumber = null;
-            currentSongFromAlbum = null;
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
       	}
     };
 
@@ -164,7 +186,8 @@ var currentlyPlayingSong = null;
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
-
+var currentSoundFile = null;
+var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
 
